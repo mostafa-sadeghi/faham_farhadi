5e401949d54ad73121257d1e6d471fe276b19fe9
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-30 16:56:25 +0330

    sadeghi

---
+++ about_while.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+# for n in numbers:
+#     print(n * 2)
+
+
+i = 0
+while i < 5:
+    print(numbers[i]*2)
+    i = i + 1   # i+=1
+
+
+name = "farhamn"
+
+# exercise 1 : کشیدن مربع و مثلت با while
