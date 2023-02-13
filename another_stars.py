30b01450eac2fe8776d24406555a38f7e4b27015
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-13 17:01:00 +0330

    sadeghi

---
+++ another_stars.py
@@ -0,0 +1,40 @@
+'''
+* * * *
+* * * *
+* * * *
+* * * *
+'''
+
+# print('* '*4)
+# print('* '*4)
+# print('* '*4)
+# print('* '*4)
+
+# for i in range(4):
+#     print('* '*4)
+
+# i = 0
+# while i < 4:
+#     print('* '*4)
+#     i = i + 1  # i +=1
+
+# for 4
+# while
+total = 0
+for i in range(4):
+    number = int(input('enter a number: '))
+    total = total + number
+
+print("total is:", total)
+
+
+# exercise:
+'''
+* * *
+* * *
+* * *
+'''
+
+"""نمره سه درس یک دانش آموز را از ورودی بگیری و معدلش رو پرینت کنی"""
+"""معدل میشه مجموع نمرات تقسیم بر تعداشون که اینجا سه هست"""
+"""با حلقه for  و while"""
--- stars.py
+++ stars.py
@@ -11,16 +11,14 @@
 # print(' '*1 + '* '*4)
 # print(' '*0 + '* '*5)
 
-for i in range(5):
-    print(' ' * (4-i) + '* ' * (i+1))
+# for i in range(5):
+#     print(' ' * (4-i) + '* ' * (i+1))
 
 # do it with while loop
-i = 0
-while i < 5:
-    print(' ' * (4-i) + '* ' * (i+1))
-    i = i + 1
-
-
+# i = 0
+# while i < 5:
+#     print(' ' * (4-i) + '* ' * (i+1))
+#     i = i + 1
 
 
 '''
@@ -32,6 +30,15 @@ while i < 5:
 '''
 
 # print
+print(' '*0 + '* '*5)
+print(' '*1 + '* '*4)
+print(' '*2 + '* '*3)
+print(' '*3 + '* '*2)
+print(' '*4 + '* '*1)
 # for
-# while
 
+for i in range(5):
+    print(' '*i + '* '*(5-i))
+
+
+# while
