08f2a1ca82bedf01c1877d7bf9fbd1bf4624f424
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-27 16:59:27 +0330

    sadeghi

---
+++ review1.py
@@ -0,0 +1,72 @@
+# print('hello')
+# print('2+3')
+# print(2+3)
+# print("your score is:", "100 + 1")
+# print("your score is:", 100 + 1)
+# print("reza said \"ballalla\"")
+# print("id\tname")
+# print("1\ttina")
+# print("hello.\nwelcome to our python class.")
+# print("hi.\rwelcome to python class.\rwelcome to pygame")
+
+# x = 10
+# print("x =", x)
+
+# score = 0
+# print("score is:", score)
+
+# score = score + 1
+# print("new score :", score)
+
+# score += 1
+# print("new score :", score)
+
+# score = score - 1
+# print("new score :", score)
+# score -= 1
+# print("new score :", score)
+
+print(4 % 2)
+print(5 % 2)
+
+# print(3/2)
+# print(3//2)
+
+
+# print(2 + 3 * 2)
+# print((2 + 3) * 2)
+
+# برنامه ای بنویس که نام و نمرات سه درس یک دانش آموز را از ورودی بگیرد
+# و معدلش رو حساب کنه.
+# name = input('enter your name: ')
+# s1 = float(input('enter first score: '))
+# s2 = float(input('enter second score: '))
+# s3 = float(input('enter third score: '))
+
+# ave = (s1 + s2 + s3) / 3
+# print(f"{name}'s average is: {ave}")
+
+
+# exercise 1:
+# برنامه ای بنویس که نام، نام خانوادگی و سن یک دانش آموز را از ورودی بگیرد
+# و به صورت زیر پرینت دهد.
+# ali rezaei is 12 years old.
+
+# a = 4
+# b = 5
+
+# if a > b:
+#     print("a is greater than b.")
+# if a < b:
+#     print("a is lower than b.")
+a = 4
+b = 4
+
+# if a >= b:
+#     print("a is greater than or equal to b.")
+# if a < b:
+#     print("a is lower than b.")
+if a > b:
+    print("a is greater than b.")
+if a <= b:
+    print("a is lower than or equal to b.")
