3a11560e93727c422e739888ffc21baac442128b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-03 16:57:26 +0330

    sadeghi

---
+++ s20.py
@@ -0,0 +1,98 @@
+# for i in range(10):
+#     print("Hi")
+#     print("Python")
+
+
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i, end=' ')
+
+
+# عدد های زوج
+# for i in range(2,11,2):
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(5):
+#     print((i+1)*2 , end=' ')
+
+# print()
+# عدد های فرد
+# for i in range(1,10,2):
+#     print(i, end=' ')
+
+
+# count down from 10 t0 1
+# for i in range(10, 0, -1):
+#     print(i, end=' ')
+
+# for i in [2,4,6,7,4,3]:
+#     print(i, end=' ')
+# print()
+
+# for i in (2,3,7,8,9,0):
+#     print(i, end=' ')
+# print()
+
+# for i in '23468901':
+#     print(i, end=' ')
+# print()
+
+
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+
+# a = 0
+# for i in range(10):
+#     a = a + 1
+
+# for j in range(10):
+#     a = a + 1
+
+# print(a)
+
+# a = 0
+# for i in range(10):
+#     a = a + 1
+#     for j in range(10):
+#         a = a + 1
+
+# print(a)
+
+
+# sum = 0
+
+# for i in range(1001):
+#     sum = sum + i
+
+# print(sum)
+
+
+# while loop
+
+# for i in range(10):
+#     print(i)
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i = i + 1
+
+# quit = 'no'
+# while quit == 'no':
+#     quit = input('Do you want to quit? ')
+
+done = False
+while not done:
+    quit = input('Do you want to quit? ')
+    if quit == 'y':
+        done = True
