4559546db89f28400f46e53d07c225e77a7724ca
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-17 16:57:20 +0330

    sadeghi

--- s22.py
+++ s22.py
@@ -1,34 +1,43 @@
-import pygame
-import random
-pygame.init()
+# import pygame
+# import random
+# pygame.init()
 
-screen = pygame.display.set_mode((700, 500))
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-screen.fill(BLACK)
-rect_x = 0
-rect_y = 0
-rect_change_x = 1
-rect_change_y = 1
+# screen = pygame.display.set_mode((700, 500))
+# WHITE = (255, 255, 255)
+# BLACK = (0, 0, 0)
+# RED = (255, 0, 0)
+# GREEN = (0, 255, 0)
+# screen.fill(WHITE)
+# rect_x = 0
+# rect_y = 0
+# rect_change_x = 1
+# rect_change_y = 1
 
-snow_list = []
+# snow_list = []
 
-for i in range(50):
-    x = random.randrange(0, 400)
-    y = random.randrange(0, 400)
-    # pygame.draw.circle(screen, RED, [x, y], 2)
-    snow_list.append([x, y])
+# for i in range(50):
+#     x = random.randrange(0, 400)
+#     y = random.randrange(0, 400)
+#     # pygame.draw.circle(screen, RED, [x, y], 2)
+#     snow_list.append([x, y])
 
 # pygame.display.flip()
 
 
-clock = pygame.time.Clock()
-done = True
-while done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = False
+# def draw_tree():
+#     pygame.draw.rect(screen, BLACK, [60, 400, 30, 45])
+#     pygame.draw.polygon(screen, GREEN, [[150, 400], [75, 250], [0, 400]])
+#     pygame.draw.polygon(screen, GREEN, [[140, 350], [75, 230], [10, 350]])
+#     pygame.display.flip()
+
+
+# draw_tree()
+# clock = pygame.time.Clock()
+# done = True
+# while done:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             done = False
 
     # pygame.draw.rect(screen, BLACK, [rect_x, rect_y, 50, 50])
     # rect_x += rect_change_x
@@ -47,15 +56,28 @@ while done:
 
     # pygame.display.flip()
 
-    for i in range(len(snow_list)):
-        pygame.draw.circle(screen, WHITE, snow_list[i], 2)
-        snow_list[i][1] += 1
+    # for i in range(len(snow_list)):
+    #     pygame.draw.circle(screen, WHITE, snow_list[i], 2)
+    #     snow_list[i][1] += 1
 
-        if snow_list[i][1] > 400:
-            y = random.randrange(-50,-10)
-            snow_list[i][1] = y
-            x = random.randrange(0,400)
-            snow_list[i][0] = x
+    #     if snow_list[i][1] > 400:
+    #         y = random.randrange(-50,-10)
+    #         snow_list[i][1] = y
+    #         x = random.randrange(0,400)
+    #         snow_list[i][0] = x
 
-    pygame.display.flip()
-    clock.tick(20)
+    # pygame.display.flip()
+    # clock.tick(20)
+
+
+
+def sum_two_numbers(a,b):
+    result = a + b
+    return result
+
+
+x = sum_two_numbers(2,3)
+print(x)
+
+
+# تابع برای تفریق، ضرب و تقسیم
